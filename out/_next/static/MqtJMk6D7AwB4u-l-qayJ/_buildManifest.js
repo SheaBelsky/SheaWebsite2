bec5687706caa673eca7cde0a108d0b810bc1a3e
Shea Belsky <sheabelsky@gmail.com>
2022-02-18 22:38:06 -0500

    Deploy Next.js to gh-pages

--- out/_next/static/MqtJMk6D7AwB4u-l-qayJ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/479-7596fd89d7f3a437.js","static/chunks/pages/index-7f611f76a6d4a30b.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
