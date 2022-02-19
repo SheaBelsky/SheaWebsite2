286199d3fb4273012ae3cfaf27360957d4a03b28
Shea Belsky <sheabelsky@gmail.com>
2022-02-18 22:15:16 -0500

    Test GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
+const debug = process.env.NODE_ENV !== 'production'
+
 const nextConfig = {
+  assetPrefix: !debug ? '/sheawebsite2/' : '',
   reactStrictMode: true,
 }
 
