c9ffeb38acfdc242c51feb732fdbeb7f76574f5c
Shea Belsky <sheabelsky@gmail.com>
2023-01-14 13:33:20 -0500

    Fix missing title on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,13 @@ import { ChakraProvider } from "@chakra-ui/react";
 import BaseLayout from "../components/base_layout";
 import chakraTheme from "../lib/theme";
 import HomeComponent from "../app/home";
+import Head from "next/head";
 
 const HomePageOld = () => (
   <ChakraProvider theme={chakraTheme}>
+    <Head>
+      <title>Shea Belsky</title>
+    </Head>
     <BaseLayout>
       <HomeComponent />
     </BaseLayout>
