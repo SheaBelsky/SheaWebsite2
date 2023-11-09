566bb9c36c80c58995d7d2058ccc0be63c4c4a80
Shea Belsky <shea@mentra.me>
2023-11-09 14:36:57 -0500

    Add new presentations

--- app/talks/talks.tsx
+++ app/talks/talks.tsx
@@ -22,6 +22,22 @@ const TalksComponent = () => {
       </Heading>
       <SimpleGrid columns={{ base: 1, md: 2 }} spacing={5} width="100%">
         <WorkRow
+          backgroundColor="yellow.500"
+          dates="November 1, 2023"
+          href="https://www.novartis.com/about/diversity-equity-inclusion/inclusivity"
+          linkVariant="basic"
+          subtitle="Novartis"
+          title="A.B.L.E. Inclusion Summit"
+        />
+        <WorkRow
+          backgroundColor="pink.700"
+          dates="October 25, 2023"
+          href="https://www.youtube.com/watch?v=BgN-ZDFULw8"
+          linkVariant="basic"
+          subtitle="Organization for Autism Research"
+          title="Autism Inclusion in the Workplace Panel (recording)"
+        />
+        <WorkRow
           backgroundColor="blue.700"
           dates="July 25, 2023"
           href="https://www.ultimatemedical.edu/about/diversity-and-inclusion/"
@@ -67,7 +83,7 @@ const TalksComponent = () => {
           href="https://www.instagram.com/p/Co7-Q9qBa3U/"
           linkVariant="basic"
           subtitle="3LPlace"
-          title="With Neurodivergent Adults and Advocates"
+          title="With Neurodivergent Adults and Advocates (recording)"
         />
         <WorkRow
           backgroundColor="#7F187F"
@@ -75,7 +91,7 @@ const TalksComponent = () => {
           href="https://www.youtube.com/watch?v=I40uwPHMDBI"
           linkVariant="basic"
           subtitle="Mentra & CodeUp"
-          title="Neurodiversity Discussion Panel"
+          title="Neurodiversity Discussion Panel (recording)"
         />
         <WorkRow
           backgroundColor="#D27CB3"
@@ -83,7 +99,7 @@ const TalksComponent = () => {
           href="https://www.youtube.com/watch?v=hgWid0UkZtw"
           linkVariant="basic"
           subtitle="Mentra"
-          title="Unmasking the World's Neurodiversity and Bringing your Authentic Self to Work"
+          title="Unmasking the World's Neurodiversity and Bringing your Authentic Self to Work (recording)"
         />
         <WorkRow
           backgroundColor="teal.400"
@@ -99,7 +115,7 @@ const TalksComponent = () => {
           href="https://www.youtube.com/watch?v=WR15vXWEo64"
           linkVariant="basic"
           subtitle="HubSpot & Raka Collective"
-          title="Becoming an A11y: Why Accessibility Matters to Everyone"
+          title="Becoming an A11y: Why Accessibility Matters to Everyone (recording)"
         />
       </SimpleGrid>
 
@@ -108,62 +124,68 @@ const TalksComponent = () => {
       </Heading>
       <SimpleGrid columns={{ base: 1, md: 2 }} spacing={5} width="100%">
         <WorkRow
+          backgroundColor="red.600"
+          dates="October 11, 2023"
+          href="https://www.youtube.com/watch?v=yC7GbcYSv1A"
+          linkVariant="basic"
+          subtitle="Gaining the Technology Leadership Edge"
+          title="Creating a Neuro Inclusive Workplace"
+        />
+        <WorkRow
           backgroundColor="cyan.600"
           dates="June 5, 2023"
           href="https://podcasters.spotify.com/pod/show/onthe-spectrum/episodes/Were-Employable---Shea-Belsky-CTO-of-Mentra--Episode-16-e259ibp/a-a9v2rlm"
           linkVariant="basic"
-          subtitle={`"We're Employable" - Shea Belsky: CTO of Mentra`}
-          title="On The Spectrum"
+          subtitle="On The Spectrum"
+          title={`"We're Employable" - Shea Belsky: CTO of Mentra`}
         />
         <WorkRow
           backgroundColor="teal.400"
           dates="April 27, 2023"
           href="https://open.spotify.com/episode/62MM2ZkkMnxVZkUGdMhQsK"
           linkVariant="basic"
-          subtitle="Shea Belsky, Mentra and Thoughts on Living Life with Autism"
-          title="Atypical Parenting"
+          subtitle="Atypical Parenting"
+          title="Shea Belsky, Mentra and Thoughts on Living Life with Autism"
         />
         <WorkRow
           backgroundColor="red.400"
           dates="March 3, 2023"
           href="https://open.spotify.com/episode/72PvUSNsJacdAQUGDuvQOC"
           linkVariant="basic"
-          subtitle="Human Resources"
-          title="Neurodiverse Noise"
+          subtitle="Neurodiverse Noise"
+          title="Human Resources"
         />
-
         <WorkRow
           backgroundColor="green.800"
           dates="February 23, 2023"
           href="https://open.spotify.com/episode/3xW5vwAGKCTw7xWp3JNBfe"
           linkVariant="basic"
-          subtitle="You're Qualified - To Be A C-Suite Exec As A Neurodivergent Person"
-          title="You're Not Qualified"
+          subtitle="You're Not Qualified"
+          title="You're Qualified - To Be A C-Suite Exec As A Neurodivergent Person"
         />
-
         <WorkRow
           backgroundColor="#120c0f"
           dates="February 1, 2023"
           href="https://anchor.fm/glich/episodes/E25---Neurodivergence-and-the-tech-industry-e1tilou"
           linkVariant="basic"
-          subtitle="Episode #25 - Neurodivergence and the tech industry"
-          title="glich"
+          subtitle="glich"
+          title="Episode #25 - Neurodivergence and the tech industry"
         />
         <WorkRow
           backgroundColor="#7d46b4"
           dates="January 16, 2023"
           href="https://anchor.fm/assemblinginclusion/episodes/027-Mentra-e1thhtr"
           linkVariant="basic"
-          subtitle="Episode #27 - Mentra"
-          title="Assembling Inclusion"
+          subtitle="Assembling Inclusion"
+          title="Episode #27 - Mentra"
         />
         <WorkRow
           backgroundColor="#1D355E"
           dates="January 4, 2021"
           href="https://anchor.fm/autism-personal-coach/episodes/Autism-Stories-Shea-Belsky-eoggf0"
           linkVariant="basic"
-          subtitle="Autism Personal Coach"
-          title="Autism Stories"
+          subtitle="Autism Stories"
+          title="Autism Personal Coach"
         />
       </SimpleGrid>
     </Fragment>
